82ac22ee4b8f00c28cb1f11b6d4bcd37e93cf417
Anmol <anmolfutela94@gmail.com>
2025-02-06 19:58:25 +0530

    added the bottstrap

---
+++ Todo-app-version-one/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <center class="todo-container">
+      <h1>ToDO App</h1>
+      <div class="row">
+        <div class="col-4">col</div>
+        <div class="col-4">col</div>
+        <div class="col-2">col</div>
+      </div>
+    </center>
+  );
+}
+
+export default App;
