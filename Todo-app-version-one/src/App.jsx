1d247b667ff0459f0b2b39c7ad4b16381d4d99d3
Anmol <anmolfutela94@gmail.com>
2025-02-07 12:34:04 +0530

    Added Components folder

--- Todo-app-version-one/src/App.jsx
+++ Todo-app-version-one/src/App.jsx
@@ -1,14 +1,41 @@
+import AppName from './components/AppName';
 function App() {
-  return (
-    <center class="todo-container">
-      <h1>ToDO App</h1>
+  return (<div className="todo-container text-center"> 
+      <AppName></AppName>
       <div class="row">
-        <div class="col-4">col</div>
-        <div class="col-4">col</div>
-        <div class="col-2">col</div>
+        <div class="col-6">
+          <input type="text" placeholder="Enter TODO Here" />
+        </div>
+        <div class="col-4">
+          <input type="date" />
+        </div>
+        <div class="col-2">
+          <button type="button" class="btn btn-success">
+            Add
+          </button>
+        </div>
       </div>
-    </center>
-  );
-}
+      <div class="row">
+        <div class="col-6">Buy Milk</div>
+        <div class="col-4">4/10/23</div>
+        <div class="col-2">
+          <button type="button" class="btn btn-danger">
+            Delete
+          </button>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-6">
+          Go TO College
+        </div>
+        <div class="col-4">
+          4/10/23
+        </div>
+        <div class="col-2">
+        <button type="button" class="btn btn-danger">Delete</button>
+        </div>
+        </div>
+           </div>);
+      }
 
 export default App;
